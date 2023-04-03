d3c0d0c4232ebd739e486a61b5ca918fec6fa100
skanga <mutinda947@gmail.com>
2023-04-03 15:08:35 +0300

    security updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); // Load environment variables from .env file
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat",
@@ -5,7 +6,7 @@ module.exports = {
     hardhat: {},
     mumbai: {
       url: "https://rpc-mumbai.matic.today",
-      accounts: ["6a93c03124c83995f4120e2ebef287dc5a2939271d28cbc1e938960346838c03"],
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
   solidity: {
